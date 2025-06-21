75f6943b85982fe99cf699bd73376276e86520cb
unknown <PC36@MAAPCB02LC3036.duoc.acad>
2025-06-21 10:47:53 -0400

    Se agrega actividad

---
+++ actividad1.py
@@ -0,0 +1,32 @@
+import os
+os.system("cls")
+
+#- programa python que solicita 5 notas
+#- guarda las 5 notas en una lista
+#- muestra el promedio (debe ser funcion)
+
+lista= []
+contador = 1
+
+def solicitar_nota():
+    while True:
+        try:
+            numero = float(input("Ingresar nota: "))
+            if numero >= 1 and numero <= 7:
+                return numero
+        except:
+            print("Nota de ser numero")
+
+def calcular_promedio (lista):
+    sumatoria = 0
+    for i in lista:
+        sumatoria+=i
+    return sumatoria/len(lista)
+
+#programa principal desde aqui
+notas = []
+for i in range(5):
+    nota = solicitar_nota()
+    notas.append(nota)
+promedio = calcular_promedio(notas)
+print(f"Promedio {promedio}")
